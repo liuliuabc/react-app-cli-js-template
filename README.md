48071a36a44d224ce7a5db5642e06684bdd7ef7e
lyh <lyh@YNSH100367.local>
2019-12-26 11:32:10 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# <cli>projectName</cli>
+
+## 项目部署与调试
+* 开发版调试 `yarn start`
+* 打包prod版 `yarn build`
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                恭喜你，<cli>projectName</cli>项目已经成功创建
+            </div>
+        )
+    }
+}
+
+export default App;
